c59ffd7a499235eb15631108be3d25fe928ce45d
DarylX <DarylX@Magenic.com>
2015-02-27 15:59:51 -0600

    Added the float button

--- app/src/main/java/com/darylx/bta/BTApplication.java
+++ app/src/main/java/com/darylx/bta/BTApplication.java
@@ -1,16 +1,12 @@
 package com.darylx.bta;
 
-import android.annotation.TargetApi;
-import android.graphics.Outline;
-import android.os.Build;
+import android.animation.Animator;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewOutlineProvider;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.darylx.bta.controllers.PageSwitchController;
@@ -20,7 +16,6 @@ public class BTApplication extends ActionBarActivity {
     PageSwitchController pageSwitcher;
     ViewPager pager;
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +25,8 @@ public class BTApplication extends ActionBarActivity {
         pager = (ViewPager)findViewById(R.id.pager);
         pager.setAdapter(pageSwitcher);
 
-        ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
-            @Override
-            public void getOutline(View view, Outline outline) {
-                // Or read size directly from the view's width/height
-                int size = getResources().getDimensionPixelSize(R.dimen.diameter);
-                outline.setOval(0, 0, size, size);
-            }
-        };
-
         ImageButton addButton = (ImageButton)findViewById(R.id.add_fab);
-        addButton.setOutlineProvider(viewOutlineProvider);
+        ViewCompat.setElevation(addButton,8);
     }
 
     @Override
